603d71ab22a58aaa4aa37edb951e389fde8c83d9
Sergey Shapovalov <scotty200305@gmail.com>
2024-05-14 19:33:16 +0400

    sprint №9

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,10 +2,62 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val editText = findViewById<EditText>(R.id.edit_text_id)
+        val closeButton = findViewById<ImageView>(R.id.close_button)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        editText.setText(savedText)
+
+        toolbar.setNavigationOnClickListener { finish() }
+
+        closeButton.setOnClickListener {
+            editText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                savedText = s.toString()
+                if (s.isNullOrEmpty()){
+                    closeButton.isVisible = false
+                } else {
+                    closeButton.isVisible = true
+                }
+            }
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        editText.addTextChangedListener(simpleTextWatcher)
     }
+
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+        const val SEARCH_TEXT_DEF = ""
+    }
+    private var savedText: String = SEARCH_TEXT_DEF
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, savedText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedText = savedInstanceState.getString(SEARCH_TEXT, SEARCH_TEXT_DEF)
+    }
+
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,11 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.MailTo
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,6 +14,35 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val shareButton = findViewById<TextView>(R.id.share_button)
+        val supportButton = findViewById<TextView>(R.id.support_button)
+        val eulaButton = findViewById<TextView>(R.id.eula_button)
+
         toolbar.setNavigationOnClickListener { finish() }
+        shareButton.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.practicum_url))
+                type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+        supportButton.setOnClickListener {
+            val supportIntent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.email_body))
+            }
+            startActivity(supportIntent)
+        }
+        eulaButton.setOnClickListener {
+            val eulaIntent = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(getString(R.string.eula_url))
+            }
+            startActivity(eulaIntent)
+        }
     }
 }
