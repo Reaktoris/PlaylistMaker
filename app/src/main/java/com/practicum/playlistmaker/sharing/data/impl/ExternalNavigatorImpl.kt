e19f34cd6733f726a389b7bae6c4bad3671aef2b
Sergey Shapovalov <scotty200305@gmail.com>
2024-11-24 15:31:39 +0400

    Sprint 16_3

--- app/src/main/java/com/practicum/playlistmaker/sharing/data/impl/ExternalNavigatorImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/sharing/data/impl/ExternalNavigatorImpl.kt
@@ -3,36 +3,36 @@ package com.practicum.playlistmaker.sharing.data.impl
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.sharing.domain.ExternalNavigator
-import com.practicum.playlistmaker.sharing.domain.model.EmailData
 
 class ExternalNavigatorImpl(private val context: Context) : ExternalNavigator {
-    override fun shareLink(link: String) {
+    override fun shareLink() {
         val shareIntent = Intent().apply {
             action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, link)
+            putExtra(Intent.EXTRA_TEXT, context.getString(R.string.practicum_url))
             type = "text/plain"
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
         context.startActivity(shareIntent)
     }
 
-    override fun openLink(link: String) {
+    override fun openLink() {
         val eulaIntent = Intent().apply {
             action = Intent.ACTION_VIEW
-            data = Uri.parse(link)
+            data = Uri.parse(context.getString(R.string.eula_url))
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
         context.startActivity(eulaIntent)
     }
 
-    override fun openEmail(emailData: EmailData) {
+    override fun openEmail() {
         val supportIntent = Intent().apply {
             action = Intent.ACTION_SENDTO
             data = Uri.parse("mailto:")
-            putExtra(Intent.EXTRA_EMAIL, arrayOf(emailData.email))
-            putExtra(Intent.EXTRA_SUBJECT, emailData.subject)
-            putExtra(Intent.EXTRA_TEXT, emailData.text)
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(context.getString(R.string.support_email)))
+            putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.email_subject))
+            putExtra(Intent.EXTRA_TEXT, context.getString(R.string.email_body))
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
         context.startActivity(supportIntent)
--- app/src/main/java/com/practicum/playlistmaker/sharing/domain/impl/SharingInteractorImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/sharing/domain/impl/SharingInteractorImpl.kt
@@ -2,43 +2,19 @@ package com.practicum.playlistmaker.sharing.domain.impl
 
 import com.practicum.playlistmaker.sharing.domain.ExternalNavigator
 import com.practicum.playlistmaker.sharing.domain.SharingInteractor
-import com.practicum.playlistmaker.sharing.domain.model.EmailData
 
 class SharingInteractorImpl(
     private val externalNavigator: ExternalNavigator
 ) : SharingInteractor {
     override fun shareApp() {
-        externalNavigator.shareLink(getShareAppLink())
+        externalNavigator.shareLink()
     }
 
     override fun openTerms() {
-        externalNavigator.openLink(getTermsLink())
+        externalNavigator.openLink()
     }
 
     override fun openSupport() {
-        externalNavigator.openEmail(getSupportEmailData())
-    }
-
-    private fun getShareAppLink(): String {
-        return LINK_SHARE
-    }
-
-    private fun getTermsLink(): String {
-        return LINK_TERMS
-    }
-
-    private fun getSupportEmailData(): EmailData {
-        return EmailData(
-            email = EMAIL,
-            subject = SUBJECT,
-            text = TEXT
-        )
-    }
-    companion object {
-        private const val LINK_SHARE = "https://practicum.yandex.ru/android-developer/"
-        private const val LINK_TERMS = "https://yandex.ru/legal/practicum_offer/"
-        private const val EMAIL = "scotty200305@gmail.com"
-        private const val SUBJECT = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
-        private const val TEXT = "Спасибо разработчикам и разработчицам за крутое приложение!"
+        externalNavigator.openEmail()
     }
 }
