9bcb80e1cf463ad708252b11f781dbae1457ccfd
Sergey Shapovalov <scotty200305@gmail.com>
2024-11-28 00:39:19 +0400

    Sprint 19_1

---
+++ app/src/main/java/com/practicum/playlistmaker/settings/ui/fragment/SettingsFragment.kt
@@ -0,0 +1,46 @@
+package com.practicum.playlistmaker.settings.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.practicum.playlistmaker.databinding.FragmentSettingsBinding
+import com.practicum.playlistmaker.settings.ui.view_model.SettingsViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class SettingsFragment : Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+    private val viewModel by viewModel<SettingsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nightModeSwitch.isChecked = viewModel.getThemeSettings()
+
+        binding.nightModeSwitch.setOnCheckedChangeListener { _, checked ->
+            viewModel.updateThemeSettings(checked)
+        }
+
+        binding.shareButton.setOnClickListener {
+            viewModel.shareApp()
+        }
+
+        binding.supportButton.setOnClickListener {
+            viewModel.openSupport()
+        }
+
+        binding.eulaButton.setOnClickListener {
+            viewModel.openTerms()
+        }
+
+    }
+}
